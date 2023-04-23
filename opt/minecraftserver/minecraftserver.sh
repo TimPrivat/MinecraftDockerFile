924809dc8479f5402348dec95186094b90425845
DockerHost <timleon2701@gmail.com>
2023-04-23 16:55:26 +0200

    läuft aber keine konfiguration

--- opt/minecraftserver/minecraftserver.sh
+++ opt/minecraftserver/minecraftserver.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+
+
+echo "Die Version ist: $MINECRAFT_VERSION_ENV"
+
+#java -jar /opt/minecraftserver/spigot-${MINECRAFT_VERSION_ENV}.jar
+
+
+java -Xms"$MEMORY_MIN_ENV" -Xmx"$MEMORY_MAX_ENV" -jar /opt/minecraftserver/spigot-"${MINECRAFT_VERSION_ENV}".jar nogui
+
+
+while true
+    do 
+    sleep 10
+done
+
+stopMinecraftServer(){
+echo "Stopping MInecraftServer..."
+
+}
+
+trap stopMinecraftServer SIGTERM
